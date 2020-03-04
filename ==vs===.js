1588b1918511fdefe4fccaf6fd749c292410785b
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-03-04 22:23:23 +0600

    double vs triple equal

---
+++ ==vs===.js
@@ -0,0 +1,15 @@
+// double equal compare the value as true or false
+
+let firstNumb = 1;
+let secondNumb = "1";
+
+firstNumb == secondNumb ? console.log("condition is true") : console.log("condition is false");
+
+// triple equal compare the value as well as the type 
+
+let one = 1;
+let two = "1";
+
+one === two ? console.log("condition is true") : console.log('condition is false')
+
+
